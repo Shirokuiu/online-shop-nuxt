b5928a53f7d40d14efa6583a62ddbf70154b6148
 Pavel <pavel@iMac-Pavel.local>
2021-12-06 18:04:13 +0300

    Установил Vue Class Component

---
+++ modules/test/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ greet }}</h1>
+    {{ fullName }}
+  </div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import Vue from 'vue'
+
+@Component({
+  name: 'Test',
+})
+export default class Test extends Vue {
+  greet = 'hello'
+  firstName = 'John'
+  lastName = 'Doe'
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Test v-if="isShow" />
+    <button type="button" @click="clickHandler">Click</button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Component from 'vue-class-component'
 
-export default Vue.extend({})
+@Component({
+  name: 'IndexPage',
+  components: {
+    Test: () => import('@/modules/test/components/Test.vue'),
+  },
+})
+export default class Index extends Vue {
+  isShow = false
+
+  clickHandler(): void {
+    this.isShow = !this.isShow
+  }
+}
 </script>
